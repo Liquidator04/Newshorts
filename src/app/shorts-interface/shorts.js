23631ecf53e0f13cf2c4b170a3b80e3981c5adb8
Liquidator04 <aritrabag04@gmail.com>
2024-03-09 01:19:22 +0530

    Finally integrated videos

--- src/app/shorts-interface/shorts.js
+++ src/app/shorts-interface/shorts.js
@@ -14,10 +14,15 @@ export default function EmblaCarousel() {
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide flex justify-center"><Image src={test1} alt="test" width={400} height={250}/></div>
-        <div className="embla__slide flex justify-center"><Image src={test2} alt="test" width={400} height={250}/></div>
-        <div className="embla__slide flex justify-center"><Image src={test3} alt="test" width={400} height={250}/></div>
-        <div className="embla__slide flex justify-center"><Image src={test4} alt="test" width={400} height={250}/></div>
+        <div className="embla__slide flex justify-center">
+          <video width="300" height="400" controls preload="none">
+            <source src="/Video/Video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="embla__slide flex justify-center"><Image src={test2} alt="test" width={300} height={400}/></div>
+        <div className="embla__slide flex justify-center"><Image src={test3} alt="test" width={300} height={400}/></div>
+        {/* <div className="embla__slide flex justify-center"><Image src={test4} alt="test" width={400} height={250}/></div> */}
       </div>
     </div>
   )
